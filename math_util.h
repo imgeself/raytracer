49e1c3704b1654b6901696ac905ffe21e7578e75
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-02-10 19:37:02 +0300

    refract function added

--- math_util.h
+++ math_util.h
@@ -7,6 +7,16 @@
 
 #define F32Max FLT_MAX
 
+inline float Clamp(float low, float value, float high) {
+    if (value < low) {
+	return low;
+    } else if (value > high) {
+	return high;
+    } else {
+	return value;
+    }
+}
+
 inline float DotProduct(const Vector3 v1, const Vector3 v2) {
   return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
@@ -37,6 +47,34 @@ float RandomUnilateral() {
 
 float RandomBilateral() {
     return 2.0f * RandomUnilateral() - 1.0f;
+}
+
+bool Refract(Vector3 incidentVector, Vector3 normal,
+	     float refractiveIndex, Vector3* refractionDirection) {
+    // Clamp cos value for avoiding any NaN errors;
+    float cosIncidentAngle = Clamp(-1.0f, DotProduct(incidentVector, normal), 1.0f);
+    Vector3 hitNormal = normal;
+    float refractiveIndexRatio;
+    // NOTE: we assume incident ray comes from the air which has refraction index equals 1
+    if (cosIncidentAngle < 0) {
+	// We are coming from outside the surface
+	cosIncidentAngle = -cosIncidentAngle;
+	refractiveIndexRatio = 1.0 / refractiveIndex;
+    } else {
+	hitNormal = -normal;
+	refractiveIndexRatio = refractiveIndex; // / 1.0f
+    }
+
+    float discriminant = 1 - refractiveIndexRatio * refractiveIndexRatio *
+	(1 - cosIncidentAngle * cosIncidentAngle);
+    if (discriminant < 0) {
+	return false;
+    } else {
+	*refractionDirection = incidentVector * refractiveIndexRatio + hitNormal *
+	    (refractiveIndexRatio * cosIncidentAngle - sqrtf(discriminant));
+	return true;
+    }
+    
 }
 
 inline uint32_t RGBPackToUInt32(Vector3 color) {
