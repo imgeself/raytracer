94cce1baef4bf5e6f409b591552204179823fa11
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-02-26 20:53:52 +0300

    make one and simple build script

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-g++ -g -Wall -std=c++11 -o bin/raytracer main.cpp image.cpp
+compileArguments="-std=c++11 -Wall"
+
+if [ "$1" == "-d" ]; then
+    # Debug build
+    compileArguments+=" -g"
+else
+    compileArguments+=" -O2"
+fi
+
+g++ $compileArguments -o bin/raytracer main.cpp image.cpp && bin/raytracer && open render.bmp
--- buildrun.sh
+++
@@ -1,3 +0,0 @@
-g++ -std=c++11 -O3 -o bin/raytracer main.cpp image.cpp
-time bin/raytracer
-open render.bmp
