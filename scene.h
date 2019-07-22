7be7b7c7fef0722ae3750facb71f7025e077146c
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-07-22 18:36:46 +0300

    add rectangle and box create helper functions, cornell box scene function

--- scene.h
+++ scene.h
@@ -44,7 +44,79 @@ struct RectangleXY {
     Matrix4 scaleMatrix;
     Matrix4 translateMatrix;
     Matrix4 rotationMatrix;
+    uint32_t materialIndex;
 };
+
+static RectangleXY CreateRectangle(Vector3 position, Vector3 scale, uint32_t materialIndex) {
+    RectangleXY result = {};
+    Matrix4 scaleMatrixRect = IdentityMatrix;
+    Matrix4 translateMatrixRect = IdentityMatrix;
+    Matrix4 rotateMatrix = IdentityMatrix;
+
+    ScaleMatrix(scaleMatrixRect, scale);
+    TranslateMatrix(translateMatrixRect, position);
+
+    result.scaleMatrix = scaleMatrixRect;
+    result.translateMatrix = translateMatrixRect;
+    result.rotationMatrix = rotateMatrix;
+    result.materialIndex = materialIndex;
+
+    return result;
+}
+
+struct Box {
+    RectangleXY rectangles[6];
+};
+
+static Box CreateBox(Vector3 position, Vector3 scale, uint32_t materialIndex) {
+    Box result = {};
+
+    Vector3 topRectPosition = position;
+    topRectPosition.y += scale.y;
+    RectangleXY topRect = CreateRectangle(topRectPosition, Vector3(scale.x, scale.z, 1.0f), materialIndex);
+    RotateMatrixXAxis(topRect.rotationMatrix, -HALF_PI);
+
+    Vector3 bottomRectPosition = position;
+    bottomRectPosition.y -= scale.y;
+    RectangleXY bottomRect = CreateRectangle(bottomRectPosition, Vector3(scale.x, scale.z, 1.0f), materialIndex);
+    RotateMatrixXAxis(bottomRect.rotationMatrix, -HALF_PI);
+
+    Vector3 rightRectPosition = position;
+    rightRectPosition.x += scale.x;
+    RectangleXY rightRect = CreateRectangle(rightRectPosition, Vector3(scale.z, scale.y, 1.0f), materialIndex);
+    RotateMatrixYAxis(rightRect.rotationMatrix, -HALF_PI);
+
+    Vector3 leftRectPosition = position;
+    leftRectPosition.x -= scale.x;
+    RectangleXY leftRect = CreateRectangle(leftRectPosition, Vector3(scale.z, scale.y, 1.0f), materialIndex);
+    RotateMatrixYAxis(leftRect.rotationMatrix, HALF_PI);
+
+    Vector3 backRectPosition = position;
+    backRectPosition.z -= scale.z;
+    RectangleXY backRect = CreateRectangle(backRectPosition, Vector3(scale.x, scale.y, 1.0f), materialIndex);
+
+    Vector3 frontRectPosition = position;
+    frontRectPosition.z += scale.z;
+    RectangleXY frontRect = CreateRectangle(frontRectPosition, Vector3(scale.x, scale.y, 1.0f), materialIndex);
+
+    result.rectangles[0] = frontRect;
+    result.rectangles[1] = backRect;
+    result.rectangles[2] = rightRect;
+    result.rectangles[3] = leftRect;
+    result.rectangles[4] = topRect;
+    result.rectangles[5] = bottomRect;
+
+    return result;
+}
+
+static void RotateBoxZAxis(Box* box, float angle) {
+    for (uint32_t rectangleIndex = 0; rectangleIndex < 4; ++rectangleIndex) {
+        RectangleXY* rect = box->rectangles + rectangleIndex;
+        Matrix4 newRotationMatrix = IdentityMatrix;
+        RotateMatrixZAxis(newRotationMatrix, angle);
+        rect->rotationMatrix = newRotationMatrix * rect->rotationMatrix;
+    }
+}
 
 struct Camera {
     Vector3 position;
@@ -238,6 +310,97 @@ World* createScene() {
     world->spheres = spheres;
     world->rectangleCount = 1;
     world->rectangles = rect;
+    world->camera = camera;
+
+    return world;
+}
+
+World* CreateCornellBoxScene() {
+    Vector3 globalUpVector = Vector3(0.0f, 1.0f, 0.0f);
+
+    Material defaultMaterial = {};
+    //defaultMaterial.emitColor = Vector3(0.1f, 0.2f, 0.4f);
+
+    Material whiteDiffuseMaterial = {};
+    whiteDiffuseMaterial.color = Vector3(0.73f, 0.73f, 0.73f);
+
+    Material greenDiffuseMaterial = {};
+    greenDiffuseMaterial.color = Vector3(0.12f, 0.45f, 0.15f);
+
+    Material redDiffuseMaterial = {};
+    redDiffuseMaterial.color = Vector3(0.65f, 0.05f, 0.05f);
+
+    Material whiteLightMaterial = {};
+    whiteLightMaterial.emitColor = Vector3(15.0f, 15.0f, 15.0f);
+
+    uint32_t materialCount = 5;
+    Material* materials = new Material[materialCount];
+    materials[0] = defaultMaterial;
+    materials[1] = whiteDiffuseMaterial;
+    materials[2] = greenDiffuseMaterial;
+    materials[3] = redDiffuseMaterial;
+    materials[4] = whiteLightMaterial;
+
+    // Initialize rectangles
+    RectangleXY lightRect = CreateRectangle(Vector3(0.0f, 7.99f, -6.0f), Vector3(2.0f, 2.0f, 1.0f), 4);
+    RotateMatrixXAxis(lightRect.rotationMatrix, -HALF_PI);
+
+    RectangleXY bottomRect = CreateRectangle(Vector3(0.0f, -8.0f, -8.0f), Vector3(8.0f, 10.0f, 1.0f), 1);
+    RotateMatrixXAxis(bottomRect.rotationMatrix, -HALF_PI);
+
+    RectangleXY rightRect = CreateRectangle(Vector3(8.0f, 0.0f, -8.0f), Vector3(10.0f, 8.0f, 1.0f), 3);
+    RotateMatrixYAxis(rightRect.rotationMatrix, -HALF_PI);
+
+    RectangleXY leftRect = CreateRectangle(Vector3(-8.0f, 0.0f, -8.0f), Vector3(10.0f, 8.0f, 1.0f), 2);
+    RotateMatrixYAxis(leftRect.rotationMatrix, HALF_PI);
+
+    RectangleXY backRect = CreateRectangle(Vector3(0.0f, 0.0f, -14.0f), Vector3(8.0f, 8.0f, 1.0f), 1);
+
+    RectangleXY topRect = CreateRectangle(Vector3(0.0f, 8.0f, -8.0f), Vector3(8.0f, 10.0f, 1.0f), 1);
+    RotateMatrixXAxis(topRect.rotationMatrix, -HALF_PI);
+
+
+    Box box = CreateBox(Vector3(2.0f, -6.0f, -3.0f), Vector3(2.0f, 2.0f, 2.0f), 1);
+    //RotateBoxZAxis(&box, -0.3f);
+
+    Box box2 = CreateBox(Vector3(-2.0f, -4.0f, -8.0f), Vector3(2.0f, 4.0f, 2.0f), 1);
+    //RotateBoxZAxis(&box2, 0.2f);
+
+    uint32_t rectangleCount = 18;
+    RectangleXY* rectangles = new RectangleXY[rectangleCount];
+    rectangles[0] = lightRect;
+    rectangles[1] = bottomRect;
+    rectangles[2] = rightRect;
+    rectangles[3] = leftRect;
+    rectangles[4] = backRect;
+    rectangles[5] = topRect;
+    rectangles[6] = box.rectangles[0];
+    rectangles[7] = box.rectangles[1];
+    rectangles[8] = box.rectangles[2];
+    rectangles[9] = box.rectangles[3];
+    rectangles[10] = box.rectangles[4];
+    rectangles[11] = box.rectangles[5];
+    rectangles[12] = box2.rectangles[0];
+    rectangles[13] = box2.rectangles[1];
+    rectangles[14] = box2.rectangles[2];
+    rectangles[15] = box2.rectangles[3];
+    rectangles[16] = box2.rectangles[4];
+    rectangles[17] = box2.rectangles[5];
+
+
+    Camera* camera = new Camera(Vector3(0.0f, 1.0f, 20.0f));
+
+    // I used raw pointers for scene objects. Freeing heap memory is callers responsibilty.
+    // TODO: I should use smart pointers for scene objects but I don't want to do that now. 
+    // Memory automatically will be freed after program terminated.
+    World* world = new World;
+    world->materialCount = materialCount;
+    world->materials = materials;
+    world->planeCount = 0;
+    world->sphereCount = 0;
+    world->sphereSoAArrayCount = 0;
+    world->rectangleCount = rectangleCount;
+    world->rectangles = rectangles;
     world->camera = camera;
 
     return world;
